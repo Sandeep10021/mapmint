92bebbdcdc15ad25162454144b6ce41b04123973
Sandeep Saurav <71926868+Sandeep10021@users.noreply.github.com>
2021-07-26 14:54:28 +0530

    Create test_service.py

---
+++ GSoC-21/WPS/test_service.py
@@ -0,0 +1,5 @@
+import zoo
+def Hello(conf,inputs,outputs):
+    outputs["Result"]["value"]=\
+            "Hello "+inputs["name"]["value"]+" from the ZOO-Project Python world !"
+    return zoo.SERVICE_SUCCEEDED
