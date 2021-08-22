f76d227844cd003284c7272b617d0f870843a783
Sandeep10021 <sandeep.saurav97@gmail.com>
2021-08-22 16:39:20 +0530

    mapmint

---
+++ PC_Generation/cmake_build/bundle/CMakeFiles/pybundle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pybundle.dir/python/pybind.cc.o"
+  "/home/user/OpenSfM/opensfm/pybundle.pdb"
+  "/home/user/OpenSfM/opensfm/pybundle.cpython-36m-x86_64-linux-gnu.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pybundle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
